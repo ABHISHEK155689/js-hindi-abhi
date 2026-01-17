19f08832f7874077305470c6572884b369d333ca
ABHISHEK PANDEY <ap9555768202@gmail.com>
2026-01-17 16:05:39 +0000

    hi

---
+++ 02_days/first.js
@@ -0,0 +1,114 @@
+// var a=20;
+// console.log(a);
+
+
+
+
+// var a=20;
+// console.log(a);
+
+
+// function sum(a,b){
+// var total=a+b;
+// function thre(x){
+//     var thre=x*x*x;
+//     return thre
+// }
+// var thre=thre(5)
+// console.log(thre)
+// return total
+// }
+// var add=sum(4,5)
+// console.log(add)
+
+
+
+// function greet(){
+//     var a="Adfar"
+//     function hello(){
+//     var x="abhishek";
+//     return x
+// }
+// console.log(hello())
+//  return a;
+// }
+// console.log(greet())
+
+
+
+
+// console.log(hello)
+//  hello()
+// function greet(){
+//     console.log("hello good morning")
+// }
+// var hello=function(){
+//     console.log("morning")
+// }
+
+
+
+// console.log(hello)
+// hello()
+
+// function greet(){
+//     console.log("hello good morning")
+// }
+
+// var hello = function(){
+//     console.log("morning")
+// }
+
+
+
+
+
+
+
+
+
+// Scope variable in js 
+
+
+
+// function add(){
+//     var a = 20;
+//     return a;
+// }
+// console.log(add())
+
+
+
+// 
+
+
+
+// {
+//     var a = 10;
+// }
+// console.log(a)
+
+
+
+
+// SCOPE GLOBAL VARIABLE 
+//  let a=10;
+//  console.log(window.a);
+
+
+
+
+// clouse function
+
+
+
+function greet(){
+    var a=10;
+    console.log(a);
+    function inner(){
+        console.log(a)
+    }
+    inner()
+
+}
+greet()
