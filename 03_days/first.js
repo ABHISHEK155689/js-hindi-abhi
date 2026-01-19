2af6d577efa0c97c1969d863ba76851487c2d04d
ABHISHEK PANDEY <ap9555768202@gmail.com>
2026-01-19 11:02:09 +0000

    hi aaray is the from

---
+++ 03_days/first.js
@@ -0,0 +1,89 @@
+// var arr=[77,55,78,99,45,76,34]
+// console.log(arr[3])
+// arr[2]=26;
+// console.log(arr)
+// console.log(arr)
+
+
+
+
+
+// var arr=[2,"Adfar",true,null,76,"hi"]
+// console.log(arr)
+
+
+// multiple diensonal array
+
+// var arr=[
+//     [12,34,56],
+//     [34,45,56],
+//     [67,78,89]
+// ]
+// console.log(arr)
+
+
+
+// length
+
+
+// var arr=[1,2,3,4,5,6,7,8,9]
+// console.log(arr)
+
+
+// indexOf()
+
+// var arr=[2,23,455,678,554,3222,1123445,667764332]
+// console.log(arr.indexOf(2))
+
+
+
+// include of array
+//  var arr=[22,3,4,5,,6,4,3,,32,2,2]
+//  console.log(arr.includes("adfar"))
+
+
+
+// push ()
+
+// var arr=[23,45,6,5,3,2,,3,4,5,34,4,3,,3,3,3,3,33,3,3,9]
+// arr.push('abhishek pandey bhumihar rai aaap se')
+// console.log(arr)
+
+// var arr=[4,5,6,7].push(8,9,10)
+// console.log(arr)
+
+
+// pop array in the array
+
+// var arr=[23,2,312,24,354,2,32]
+// console.log(arr.pop())
+// console.log(arr)
+
+
+
+// unshift of the array
+// var arr=[23,3,4,5,3,2,,3,4,3,4,6]
+// var c=arr.unshift("a","b","c")
+// console.log(c)
+
+
+// shift the array in the index 
+
+// var arr=[23,2,312,24,354,2,32]
+// console.log(arr.shift())
+// console.log(arr)
+
+
+// splice
+
+// var arr=[23,2,312,24,354,2,320]
+// // var c=arr.splice(2,2,"a","b","c","d","e")
+// var c=arr.splice(2)
+// console.log(c)
+
+
+// slice
+
+// var arr=[2,3,4,5,6,7]
+// var arr1=arr.splice(2)
+// console.log(arr1)
