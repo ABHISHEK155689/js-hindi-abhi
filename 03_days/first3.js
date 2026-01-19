8c421de50531851c619bc3cb4ff73968bd5e9f82
ABHISHEK PANDEY <ap9555768202@gmail.com>
2026-01-19 13:00:05 +0000

    array

---
+++ 03_days/first3.js
@@ -0,0 +1,75 @@
+// sorted array in the sort 
+// var arr=[9,1,2,3,4,5,6,-100,-566,12,123]
+// arr.sort((a,b)=>{ 
+// return a-b
+// })
+// console.log(arr)
+
+
+// find method
+
+// var arr = [9,1,2,3,4,5,6,-100,-566,12,123]
+// var a=arr.find((item,index)=>{
+//     return item%2==0
+// })
+// console.log(a)
+
+
+// findIndex array 
+// var arr=[3,4,5,6,7,78,9]
+// var a=arr.findIndex((item,index)=>{
+//     return item%2==0
+// })
+// console.log(a)
+
+
+// some method
+
+// var arr=[1,2,3,4,5,6,7,8]
+// var a=arr.some((item,index)=>{
+//     return item>6
+// })
+// console.log(a)
+
+
+// every method in the array
+
+// var arr=[1,2,3,4,5,6,7,8]
+// var a=arr.every((item,index)=>{
+//     return item>6
+// })
+// console.log(a)
+
+
+// reverse array in the method og array 
+
+// var arr=[1,2,3,4,5,7]
+// var a=arr.reverse()
+// console.log(a)
+
+
+// join the method of the array 
+
+// var arr=["a","b","c","d"]
+// var str=arr.join(" _")
+// var arr1=str.split("")
+// console.log(arr1)
+// console.log(str)
+
+
+// conact()
+
+
+// var arr1=[1,2,3]
+// var arr2=[2,3,4]
+// var arr3=[5,6,7]
+// var arr4=arr1.concat(arr3,arr2,arr3)
+// console.log(arr4)
+
+
+// flat()
+
+var arr=[[12,3,4],
+[5,6,7],
+["a"]]
+console.log(arr)
