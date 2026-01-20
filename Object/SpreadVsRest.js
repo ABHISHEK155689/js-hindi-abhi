f4427b8c142435fd34035f82830c11539d38d14d
ABHISHEK PANDEY <ap9555768202@gmail.com>
2026-01-20 08:35:00 +0000

    hi adding

---
+++ Object/SpreadVsRest.js
@@ -0,0 +1,28 @@
+// spread opertaor
+
+// var obj={
+//     name: "abhishek",
+//     age:27,
+// }
+// var obj2={
+//     city: "gorakpur",
+//     height: 5,
+//     ...obj
+// }
+// console.log(obj2)
+
+
+
+// var db=[4,5,6]
+// function add (a,b,c){
+//     console.log(a+b+c)
+// }
+// add(...db)
+
+
+
+function add(...num){
+    console.log(num)
+
+}
+add(3,23,34,345,33,222,1234,223,333223,2,32223,2322,33234,)
