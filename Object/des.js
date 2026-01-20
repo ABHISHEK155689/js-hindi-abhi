9590063dbd5bba415983ae419040592fe3c99c12
ABHISHEK PANDEY <ap9555768202@gmail.com>
2026-01-20 08:45:25 +0000

    hi

---
+++ Object/des.js
@@ -0,0 +1,44 @@
+// var person={
+//     fName: "abhishek",
+//     lName: "pandey",
+//     age: 28,
+//     sayMyName: function(){
+//         console.log(this.fName+" "+this.lName)
+//     },
+//     sayMyAge: ()=>{
+//         console.log(this.age)
+//     }
+// }
+
+// var {fName,age}=person;
+// console.log(fName)
+// console.log(lName)
+
+
+// var person = {
+//     fName: "abhishek",
+//     lName: "pandey",
+//     age: 28,
+//     sayMyName: function () {
+//         console.log(this.fName + " " + this.lName)
+//     },
+//     sayMyAge: function () {
+//         console.log(this.age)
+//     }
+// };
+
+// var { fName, lName, age } = person;
+
+// console.log(fName);   // abhishek
+// console.log(lName);   // pandey
+
+// person.sayMyName();   // abhishek pandey
+// person.sayMyAge();    // 28
+
+
+
+
+// var arr=[4,5,6,7,8,9,10,11,12,13,14,15]
+// for(let item of arr){
+//     console.log(item)
+// }
