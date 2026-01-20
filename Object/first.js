19794558f8eb08fbcc732f40f61965577e998e8f
ABHISHEK PANDEY <ap9555768202@gmail.com>
2026-01-20 07:17:11 +0000

    new file adding object

---
+++ Object/first.js
@@ -0,0 +1,16 @@
+var person={
+    fName: "abhishek pandey",
+    age: 24,
+    isMarried: false,
+    city: "gorakhpur",
+    qualification: "b.tech",
+    Streem: "cse"
+}
+// person.height=6   // new properties adding methods
+delete person.age
+console.log(person)
+
+// console.log(person)
+// console.log(person.fName)
+// console.log(person["age"])
+
