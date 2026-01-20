95510a864b84416b6cd53d2546053f8854f452a2
ABHISHEK PANDEY <ap9555768202@gmail.com>
2026-01-20 09:10:33 +0000

    hi

---
+++ Object/for.js
@@ -0,0 +1,13 @@
+// var perso = {
+//     fName: "abhishek",
+//     lName: "Rashed",
+//     age: 28
+
+// }
+
+// for(let key in perso){
+//     console.log(key)
+//     console.log(perso[key])
+// }
+// var a=Object.getOwnPropertyDescriptor(perso)
+// console.log(a)
